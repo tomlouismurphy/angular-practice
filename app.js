9e42ddd6cbcb98a0b61889957a2c8e904e6218f5
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-11-20 10:42:57 -0600

    completed basic functionality

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const app = angular.module('ToDo', []);
 
 app.controller('MainController', [function(){
 	this.test = 'test';
+	this.tasks = ['apply for credit card', 'pick up turkey', 'finish stage 1 of react app'];
+	this.newTask = '';
+	this.handleSubmit = function(x){
+		x = this.newTask;
+		this.tasks.push(x);
+		console.log(this.tasks);
+	}
 }])
